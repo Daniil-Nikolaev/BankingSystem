3b6d83e2171829632707240f98bede885ce95b86
Daniil <daniillalala14@gmail.com>
2025-05-05 00:14:33 +0300

    Добавил основные зависимости и docker-compose

--- account-service/src/main/java/by/aston/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/by/aston/accountservice/AccountServiceApplication.java
@@ -3,8 +3,8 @@ package by.aston.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
