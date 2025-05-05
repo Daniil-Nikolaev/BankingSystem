b0531002d981e830b82e7fd1eb03070a4281b68d
eugenshima <eugenshima@mail.ru>
2025-05-05 20:27:03 +0300

    test commit(only comment on user-service)

--- user-service/src/main/java/by/aston/userservice/UserServiceApplication.java
+++ user-service/src/main/java/by/aston/userservice/UserServiceApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class UserServiceApplication {
-
+    //Test comment 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
